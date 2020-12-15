bc7e19f8ede1d9f5967bac6d03120a148ffc6599
Saptarshi Mallick <sapmallick@gmail.com>
2020-12-14 21:42:24 -0700

    Added move Toxic

--- src/main/java/com/calculusmaster/pokecord/game/moves/poison/Toxic.java
+++ src/main/java/com/calculusmaster/pokecord/game/moves/poison/Toxic.java
@@ -1,20 +1,21 @@
 package com.calculusmaster.pokecord.game.moves.poison;
 
 import com.calculusmaster.pokecord.game.Pokemon;
+import com.calculusmaster.pokecord.game.enums.elements.StatusCondition;
 import com.calculusmaster.pokecord.game.moves.Move;
 
 public class Toxic extends Move
 {
+    //TODO: This is a custom implemented class (normally does Badly Poisoned)
     public Toxic()
     {
         super("Toxic");
-        this.setWIP();
     }
 
     @Override
     public String logic(Pokemon user, Pokemon opponent)
     {
-        //TODO: Special Conditions
-        return "ERROR";
+        if(opponent.getStatusCondition().equals(StatusCondition.NORMAL)) opponent.setStatusCondition(StatusCondition.POISONED);
+        return opponent.getName() + " was inflicted with Poison!";
     }
 }
