4333fd2f01d11655f94785e040b2ab2480231af5
Saptarshi Mallick <sapmallick@gmail.com>
2021-05-17 16:52:26 -0700

    Fixed critical chance

--- src/main/java/com/calculusmaster/pokecord/game/MoveNew.java
+++ src/main/java/com/calculusmaster/pokecord/game/MoveNew.java
@@ -132,7 +132,7 @@ public class MoveNew
         //Modifier = Targets * Weather * Badge * Critical * Random * STAB * Type * Burn * Other
         //Ignored Components: Targets, Weather, Badge, Other
         //TODO: Weather
-        double critical = (r.nextInt(10000) < 625) ? 1.5 : 1.0;
+        double critical = user.isCrit() ? 1.5 : 1.0;
         double random = (r.nextInt(16) + 85.0) / 100.0;
         double stab = user.getType()[0].equals(this.type) || user.getType()[1].equals(this.type) ? 1.5 : 1.0;
         double type = TypeEffectiveness.getCombinedMap(opponent.getType()[0], opponent.getType()[1]).get(this.type);
