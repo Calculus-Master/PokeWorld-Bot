af82ff49471903cbed1eedc839024ade23da8946
Saptarshi Mallick <sapmallick@gmail.com>
2021-05-19 07:09:21 -0700

    Shortened moves list in duels

--- src/main/java/com/calculusmaster/pokecord/commands/moves/CommandMoves.java
+++ src/main/java/com/calculusmaster/pokecord/commands/moves/CommandMoves.java
@@ -1,6 +1,7 @@
 package com.calculusmaster.pokecord.commands.moves;
 
 import com.calculusmaster.pokecord.commands.Command;
+import com.calculusmaster.pokecord.game.Duel;
 import com.calculusmaster.pokecord.game.Move;
 import com.calculusmaster.pokecord.game.Pokemon;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -20,8 +21,11 @@ public class CommandMoves extends Command
         StringBuilder movesList = new StringBuilder().append("**Learned Moves: **\n");
         for(int i = 0; i < 4; i++) movesList.append("Move " + (i + 1) + ": " + selected.getLearnedMoves().get(i) + "\n");
 
-        movesList.append("\n**All Moves: **\n");
-        for(String s : selected.getAllMoves()) movesList.append(s + (selected.getAvailableMoves().contains(s) && (Move.isMove(s) && !Move.WIP_MOVES.contains(s)) ? "" : " :lock: ") + "\n");
+        if(!Duel.isInDuel(this.player.getId()))
+        {
+            movesList.append("\n**All Moves: **\n");
+            for (String s : selected.getAllMoves()) movesList.append(s + (selected.getAvailableMoves().contains(s) && (Move.isMove(s) && !Move.WIP_MOVES.contains(s)) ? "" : " :lock: ") + "\n");
+        }
 
         this.embed.setDescription(movesList.toString());
         this.embed.setTitle("Level " + selected.getLevel() + " " + selected.getName());
