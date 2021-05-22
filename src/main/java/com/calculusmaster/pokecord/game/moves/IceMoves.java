3f44de1f59f76f8fd3bb9bb13849bdcd22c61e19
Saptarshi Mallick <sapmallick@gmail.com>
2021-05-22 00:12:50 -0700

    Added Ice Beam

--- src/main/java/com/calculusmaster/pokecord/game/moves/IceMoves.java
+++ src/main/java/com/calculusmaster/pokecord/game/moves/IceMoves.java
@@ -69,4 +69,13 @@ public class IceMoves
 
         return move.getDamageResult(opponent, damage) + " Icicle Spear hit " + times + " time" + (times > 1 ? "s!" : "!");
     }
+
+    public String IceBeam(Pokemon user, Pokemon opponent, Duel duel, Move move)
+    {
+        boolean freeze = new Random().nextInt(100) < 10;
+
+        if(freeze) opponent.addStatusCondition(StatusCondition.FROZEN);
+
+        return Move.simpleDamageMove(user, opponent, duel, move) + (freeze ? " " + opponent.getName() + " is now frozen!" : "");
+    }
 }
