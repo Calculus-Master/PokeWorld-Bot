a201f7df6ab259ffa4a1d25565a61329cd8bc06c
Saptarshi Mallick <sapmallick@gmail.com>
2021-05-28 08:16:58 -0700

    Added Zeraora

--- src/main/java/com/calculusmaster/pokecord/util/PokemonRarity.java
+++ src/main/java/com/calculusmaster/pokecord/util/PokemonRarity.java
@@ -324,7 +324,7 @@ public class PokemonRarity
         PokemonRarity.add("Naganadel", Rarity.LEGENDARY);
         PokemonRarity.add("Stakataka", Rarity.LEGENDARY);
         PokemonRarity.add("Blacephalon", Rarity.LEGENDARY);
-
+        PokemonRarity.add("Zeraora", Rarity.LEGENDARY);
         PokemonRarity.add("Meltan", Rarity.PLATINUM);
         PokemonRarity.add("Melmetal", Rarity.MYTHICAL);
 
