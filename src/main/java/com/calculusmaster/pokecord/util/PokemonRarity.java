13e6822e8aad1c7dafacb5bf164c4f5110fbbcc2
Saptarshi Mallick <sapmallick@gmail.com>
2021-05-28 00:18:45 -0700

    Added Celesteela

--- src/main/java/com/calculusmaster/pokecord/util/PokemonRarity.java
+++ src/main/java/com/calculusmaster/pokecord/util/PokemonRarity.java
@@ -312,6 +312,7 @@ public class PokemonRarity
         PokemonRarity.add("Buzzwole", Rarity.LEGENDARY);
         PokemonRarity.add("Pheromosa", Rarity.LEGENDARY);
         PokemonRarity.add("Xurkitree", Rarity.LEGENDARY);
+        PokemonRarity.add("Celesteela", Rarity.LEGENDARY);
 
         PokemonRarity.add("Necrozma", Rarity.LEGENDARY);
         PokemonRarity.add("Dusk Mane Necrozma", Rarity.EXTREME);
