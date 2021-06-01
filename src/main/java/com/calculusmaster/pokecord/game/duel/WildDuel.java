f015e89781a888f17573128173056678a3084488
Saptarshi Mallick <sapmallick@gmail.com>
2021-06-01 09:54:03 -0700

    Fixed Wild Pokemon stat buffs

--- src/main/java/com/calculusmaster/pokecord/game/duel/WildDuel.java
+++ src/main/java/com/calculusmaster/pokecord/game/duel/WildDuel.java
@@ -175,7 +175,7 @@ public class WildDuel extends Duel
         if(pokemon.equals("")) this.players[1] = new WildPokemon(this.players[0].active.getLevel() + levelBuff);
         else this.players[1] = new WildPokemon(pokemon, this.players[0].active.getLevel() + levelBuff);
 
-        this.players[1].active.statBuff = Math.random() * 1.5 + 1;
+        this.players[1].active.statBuff = this.players[1].active.getLevel() > 60 ? Math.random() * 1.5 + 1 : 1;
         this.players[1].active.setLevel(this.players[0].active.getLevel());
         this.players[1].active.setHealth(this.players[1].active.getStat(Stat.HP));
     }
