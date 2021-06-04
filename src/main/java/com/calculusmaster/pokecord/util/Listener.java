0d41ab7e5e73b8dadbdd8000ef0d7aaa462e2af1
Saptarshi Mallick <sapmallick@gmail.com>
2021-06-04 08:18:52 -0700

    Increased redeem chance slightly

--- src/main/java/com/calculusmaster/pokecord/util/Listener.java
+++ src/main/java/com/calculusmaster/pokecord/util/Listener.java
@@ -276,7 +276,7 @@ public class Listener extends ListenerAdapter
 
     private static void redeemEvent(MessageReceivedEvent event)
     {
-        if(new Random().nextInt(10000) < 1)
+        if(new Random().nextInt(7500) < 1)
         {
             PlayerDataQuery p = new PlayerDataQuery(event.getAuthor().getId());
             p.changeRedeems(1);
