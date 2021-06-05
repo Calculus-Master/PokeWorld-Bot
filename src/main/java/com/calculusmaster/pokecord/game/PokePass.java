ad9049bf8d987b29aebecbbfbd349df24f8f9f0b
Saptarshi Mallick <sapmallick@gmail.com>
2021-06-05 12:32:54 -0700

    Fixed p!pokepass part 2

--- src/main/java/com/calculusmaster/pokecord/game/PokePass.java
+++ src/main/java/com/calculusmaster/pokecord/game/PokePass.java
@@ -9,44 +9,42 @@ import java.util.Map;
 
 public class PokePass
 {
-    public Map<Integer, TierReward> passTiers = new HashMap<>();
+    public static Map<Integer, TierReward> passTiers = new HashMap<>();
     public static final int TIER_EXP = 50000;
 
     public static void init()
     {
-        PokePass pass = new PokePass();
-
         //TODO: Complete PokePass, Limit Certain Rewards based on Gym Level
-        pass.addTier(0, new EmptyReward());
-        pass.addTier(1, new CreditReward(250));
-        pass.addTier(2, new CreditReward(250));
-        pass.addTier(3, new ItemReward(PokeItem.TRADE_EVOLVER));
-        pass.addTier(4, new CreditReward(250));
-        pass.addTier(5, new CreditReward(250));
-        pass.addTier(6, new PokemonReward(15, "Mudkip", "Oshawott", "Squirtle", "Totodile", "Popplio", "Piplup", "Froakie"));
-        pass.addTier(7, new CreditReward(250));
-        pass.addTier(8, new CreditReward(250));
-        pass.addTier(9, new CreditReward(250));
-        pass.addTier(10, new ItemReward(PokeItem.EV_REALLOCATOR));
+        PokePass.addTier(0, new EmptyReward());
+        PokePass.addTier(1, new CreditReward(250));
+        PokePass.addTier(2, new CreditReward(250));
+        PokePass.addTier(3, new ItemReward(PokeItem.TRADE_EVOLVER));
+        PokePass.addTier(4, new CreditReward(250));
+        PokePass.addTier(5, new CreditReward(250));
+        PokePass.addTier(6, new PokemonReward(15, "Mudkip", "Oshawott", "Squirtle", "Totodile", "Popplio", "Piplup", "Froakie"));
+        PokePass.addTier(7, new CreditReward(250));
+        PokePass.addTier(8, new CreditReward(250));
+        PokePass.addTier(9, new CreditReward(250));
+        PokePass.addTier(10, new ItemReward(PokeItem.EV_REALLOCATOR));
     }
 
     public static String getTierDescription(int tier)
     {
-        return new PokePass().passTiers.get(tier).getTierDescription();
+        return passTiers.get(tier).getTierDescription();
     }
 
     public static boolean tierExists(int tier)
     {
-        return new PokePass().passTiers.containsKey(tier);
+        return passTiers.containsKey(tier);
     }
 
-    public void addTier(int tier, TierReward reward)
+    public static void addTier(int tier, TierReward reward)
     {
-        this.passTiers.put(tier, reward);
+        passTiers.put(tier, reward);
     }
 
-    public void reward(int tier, PlayerDataQuery player)
+    public static void reward(int tier, PlayerDataQuery player)
     {
-        this.passTiers.get(tier).grantReward(player);
+        passTiers.get(tier).grantReward(player);
     }
 }
