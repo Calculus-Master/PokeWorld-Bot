5d8c8d168397b93a10c31bacbc8896dc121e8a14
Saptarshi Mallick <sapmallick@gmail.com>
2021-06-06 11:59:00 -0700

    p!info no longer has blank lines for TM and TR if the pokemon doesn't have a TM or TR

--- src/main/java/com/calculusmaster/pokecord/commands/pokemon/CommandInfo.java
+++ src/main/java/com/calculusmaster/pokecord/commands/pokemon/CommandInfo.java
@@ -36,8 +36,8 @@ public class CommandInfo extends Command
         String type = "Type: " + (chosen.getType()[0].equals(chosen.getType()[1]) ? Global.normalCase(chosen.getType()[0].toString()) : Global.normalCase(chosen.getType()[0].toString()) + " | " + Global.normalCase(chosen.getType()[1].toString()));
         String nature = "Nature: " + Global.normalCase(chosen.getNature().toString());
         String item = "Held Item: " + PokeItem.asItem(chosen.getItem()).getStyledName();
-        String tm = chosen.hasTM() ? "TM: TM" + (chosen.getTM() < 10 ? "0" : "") + chosen.getTM()  + " - " + TM.get(chosen.getTM()).getMoveName() : "";
-        String tr = chosen.hasTR() ? "TR: TR" + (chosen.getTR() < 10 ? "0" : "") + chosen.getTR()  + " - " + TR.get(chosen.getTR()).getMoveName() : "";
+        String tm = chosen.hasTM() ? "TM: TM" + (chosen.getTM() < 10 ? "0" : "") + chosen.getTM()  + " - " + TM.get(chosen.getTM()).getMoveName() : "TM: None";
+        String tr = chosen.hasTR() ? "TR: TR" + (chosen.getTR() < 10 ? "0" : "") + chosen.getTR()  + " - " + TR.get(chosen.getTR()).getMoveName() : "TR: None";
         String stats = getStatsFormatted(chosen);
 
         this.embed.setTitle(title);
