d424ed5d5774622d5bc7d5954ce4588210377e5f
Saptarshi Mallick <sapmallick@gmail.com>
2021-06-23 11:32:50 -0700

    Added Primal weathers (unused)

--- src/main/java/com/calculusmaster/pokecord/game/enums/elements/Weather.java
+++ src/main/java/com/calculusmaster/pokecord/game/enums/elements/Weather.java
@@ -6,7 +6,10 @@ public enum Weather
     HARSH_SUNLIGHT ("Harsh sunlight roasts the battlefield!"),
     RAIN ("There is a heavy rainstorm!"),
     SANDSTORM("There is an intense sandstorm!"),
-    HAIL("There is a freezing hailstorm!");
+    HAIL("There is a freezing hailstorm!"),
+    EXTREME_HARSH_SUNLIGHT("Extreme harsh sunlight envelops the field!"),
+    HEAVY_RAIN("There is an extreme rainstorm!"),
+    DELTA_STREAM("Strong winds appeared!");
 
     private String status;
     Weather(String status)
