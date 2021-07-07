6aa6edf559222eb0b7a9132d787d7a2058cfcfaa
Saptarshi Mallick <sapmallick@gmail.com>
2021-07-06 18:43:35 -0700

    Meme

--- src/main/java/com/calculusmaster/pokecord/util/listener/MiscListener.java
+++ src/main/java/com/calculusmaster/pokecord/util/listener/MiscListener.java
@@ -2,8 +2,13 @@ package com.calculusmaster.pokecord.util.listener;
 
 import com.calculusmaster.pokecord.mongo.ServerDataQuery;
 import net.dv8tion.jda.api.events.channel.text.TextChannelDeleteEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MiscListener extends ListenerAdapter
 {
@@ -17,6 +22,28 @@ public class MiscListener extends ListenerAdapter
         if(serverData.getSpawnChannels().contains(channelID))
         {
             serverData.removeSpawnChannel(channelID);
+        }
+    }
+
+    private static final List<String> memeList = Arrays.asList("423603232971948044", "292377898655154186", "304329099604918276", "272377141813968897");
+
+    @Override
+    public void onGuildVoiceJoin(@NotNull GuildVoiceJoinEvent event)
+    {
+        if(memeList.contains(event.getMember().getId()))
+        {
+            event.getMember().mute(true).queue();
+            event.getMember().deafen(true).queue();
+        }
+    }
+
+    @Override
+    public void onGuildVoiceMove(@NotNull GuildVoiceMoveEvent event)
+    {
+        if(memeList.contains(event.getMember().getId()))
+        {
+            event.getMember().mute(true).queue();
+            event.getMember().deafen(true).queue();
         }
     }
 }
