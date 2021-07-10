e12eba19e8019febeb37c6d59cb3d36406636f78
Saptarshi Mallick <sapmallick@gmail.com>
2021-07-10 10:40:58 -0700

    Added p!bal @player to see another player's balance

--- src/main/java/com/calculusmaster/pokecord/commands/economy/CommandBalance.java
+++ src/main/java/com/calculusmaster/pokecord/commands/economy/CommandBalance.java
@@ -1,6 +1,7 @@
 package com.calculusmaster.pokecord.commands.economy;
 
 import com.calculusmaster.pokecord.commands.Command;
+import com.calculusmaster.pokecord.mongo.PlayerDataQuery;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class CommandBalance extends Command
@@ -13,14 +14,20 @@ public class CommandBalance extends Command
     @Override
     public Command runCommand()
     {
+        String targetID = this.mentions.size() > 0 ? this.mentions.get(0).getId() : this.player.getId();
 
-        int credits = this.playerData.getCredits();
-        int redeems = this.playerData.getRedeems();
+        if(!PlayerDataQuery.isRegistered(targetID))
+        {
+            this.sendMsg(this.mentions.get(0).getEffectiveName() + " is not registered!");
+            return this;
+        }
 
-        String creditsLine = "You currently have " + credits + " credits!";
+        PlayerDataQuery p = targetID.equals(this.player.getId()) ? this.playerData : new PlayerDataQuery(targetID);
 
-        String redeemsLine = "You have " + redeems + " redeem" + (redeems != 1 ? "s" : "") + "!";
+        String creditsLine = "Credits: **" + p.getCredits() + "**!";
+        String redeemsLine = "Redeems: **" + p.getRedeems() + "**!";
 
+        this.embed.setTitle(p.getUsername() + "'s Balance");
         this.embed.setDescription(creditsLine + "\n\n" + redeemsLine);
         this.embed.setThumbnail("https://images-ext-2.discordapp.net/external/xlEcYc2ErW6-vD7-nHbk3pv2u4sNwjDVx3jFEL6w9fc/https/emojipedia-us.s3.amazonaws.com/thumbs/120/emoji-one/104/money-bag_1f4b0.png");
 
