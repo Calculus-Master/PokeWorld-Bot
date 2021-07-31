d2086c2db18ec777a96e9fae4e63da9b83f32010
Saptarshi Mallick <sapmallick@gmail.com>
2021-07-30 17:53:59 -0700

    Fixed AI Pokemon Health Bars in duels

--- src/main/java/com/calculusmaster/pokecord/game/duel/extension/WildDuel.java
+++ src/main/java/com/calculusmaster/pokecord/game/duel/extension/WildDuel.java
@@ -122,7 +122,12 @@ public class WildDuel extends Duel
     @Override
     protected String getHB(int p)
     {
-        StringBuilder sb = new StringBuilder().append(this.players[p].data.getUsername()).append("'s ").append(this.players[p].active.getName()).append(": ");
+        StringBuilder sb = new StringBuilder();
+
+        if(this.isNonBotPlayer(p)) sb.append(this.players[p].data.getUsername()).append("'s ");
+        else sb.append("The Wild ");
+
+        sb.append(this.players[p].active.getName()).append(": ");
 
         if(this.players[p].active.isFainted()) sb.append("FAINTED");
         else sb.append(this.players[p].active.getHealth()).append(" / ").append(this.players[p].active.getStat(Stat.HP)).append(" HP ").append(this.players[p].active.getActiveStatusConditions());
