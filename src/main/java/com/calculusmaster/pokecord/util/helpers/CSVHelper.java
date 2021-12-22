4a3a47b2cb3830badf1329bdc86c24af41937f5e
Calculus-Master <sapmallick@gmail.com>
2021-12-22 16:04:53 -0700

    Deprecated CSVHelper

--- src/main/java/com/calculusmaster/pokecord/util/helpers/CSVHelper.java
+++ src/main/java/com/calculusmaster/pokecord/util/helpers/CSVHelper.java
@@ -18,6 +18,7 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Deprecated
 public class CSVHelper
 {
     private static List<String[]> pokemonInfo = new ArrayList<>();
