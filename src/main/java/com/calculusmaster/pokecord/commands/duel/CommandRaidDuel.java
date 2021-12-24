fdd658d72d842f7bba103f482760ee6f3f55face
Calculus-Master <sapmallick@gmail.com>
2021-12-24 12:42:14 -0700

    Updated CommandRaidDuel

--- src/main/java/com/calculusmaster/pokecord/commands/duel/CommandRaidDuel.java
+++ src/main/java/com/calculusmaster/pokecord/commands/duel/CommandRaidDuel.java
@@ -1,7 +1,6 @@
 package com.calculusmaster.pokecord.commands.duel;
 
 import com.calculusmaster.pokecord.commands.Command;
-import com.calculusmaster.pokecord.commands.CommandInvalid;
 import com.calculusmaster.pokecord.game.player.level.PlayerLevel;
 import com.calculusmaster.pokecord.util.helpers.event.RaidEventHelper;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -21,10 +20,10 @@ public class CommandRaidDuel extends Command
 
         if(join || leave)
         {
-            if(!RaidEventHelper.hasRaid(this.server.getId())) this.sendMsg("There are no active Raid duels in this server!");
+            if(!RaidEventHelper.hasRaid(this.server.getId())) this.response = "There are no active Raid duels in this server!";
             else if(this.playerData.getLevel() < PlayerLevel.REQUIRED_LEVEL_RAID) this.invalidMasteryLevel(PlayerLevel.REQUIRED_LEVEL_RAID, "to participate in Raids");
-            else if(join && RaidEventHelper.isPlayerReady(this.server.getId(), this.player.getId())) this.sendMsg("You are already ready for this server's active Raid duel! To leave, type `p!raidduel leave`.");
-            else if(leave && !RaidEventHelper.isPlayerReady(this.server.getId(), this.player.getId())) this.sendMsg("You are not in this server's active Raid duel! To join, type `p!raidduel join`.");
+            else if(join && RaidEventHelper.isPlayerReady(this.server.getId(), this.player.getId())) this.response = "You are already ready for this server's active Raid duel! To leave, type `p!raidduel leave`.";
+            else if(leave && !RaidEventHelper.isPlayerReady(this.server.getId(), this.player.getId())) this.response = "You are not in this server's active Raid duel! To join, type `p!raidduel join`.";
             else
             {
                 if(join)
@@ -32,17 +31,17 @@ public class CommandRaidDuel extends Command
                     RaidEventHelper.getRaid(this.server.getId()).setEvent(this.event);
                     RaidEventHelper.getRaid(this.server.getId()).addPlayer(this.player.getId());
 
-                    this.sendMsg("You have joined the Raid with " + this.playerData.getSelectedPokemon().getName() + "!");
+                    this.response = "You have joined the Raid with " + this.playerData.getSelectedPokemon().getName() + "!";
                 }
                 else if(leave)
                 {
                     RaidEventHelper.getRaid(this.server.getId()).removePlayer(this.player.getId());
 
-                    this.sendMsg("You have left the Raid!");
+                    this.response = "You have left the Raid!";
                 }
             }
         }
-        else this.sendMsg(CommandInvalid.getShort());
+        else this.invalid();
 
         return this;
     }
