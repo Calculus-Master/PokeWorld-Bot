49fb54a848b8946995b161348ac38a999e65bb57
Calculus-Master <sapmallick@gmail.com>
2021-12-25 14:19:55 -0700

    Updated CommandBreed

--- src/main/java/com/calculusmaster/pokecord/commands/pokemon/CommandBreed.java
+++ src/main/java/com/calculusmaster/pokecord/commands/pokemon/CommandBreed.java
@@ -40,8 +40,8 @@ public class CommandBreed extends Command
             int num1 = this.getInt(1);
             int num2 = this.getInt(2);
 
-            if(num1 < 1 || num1 > this.playerData.getPokemonList().size() || num2 < 1 || num2 > this.playerData.getPokemonList().size()) this.sendMsg("Invalid Pokemon number!");
-            else if(num1 == num2) this.sendMsg("You cannot breed a Pokemon with itself!");
+            if(num1 < 1 || num1 > this.playerData.getPokemonList().size() || num2 < 1 || num2 > this.playerData.getPokemonList().size()) this.response = "Invalid Pokemon number!";
+            else if(num1 == num2) this.response = "You cannot breed a Pokemon with itself!";
             else
             {
                 Pokemon parent1 = Pokemon.build(this.playerData.getPokemonList().get(num1 - 1));
@@ -51,13 +51,13 @@ public class CommandBreed extends Command
 
                 boolean validEggGroup = parent1.getEggGroup().stream().anyMatch(e1 -> parent2.getEggGroup().stream().anyMatch(e1::equals));
 
-                if(this.playerData.getOwnedEggIDs().size() >= PokemonEgg.MAX_EGGS) this.sendMsg("You have the maximum number of eggs! To breed more, hatch some of your existing eggs!");
-                else if(UNABLE_TO_BREED.contains(parent1.getUUID()) || UNABLE_TO_BREED.contains(parent2.getUUID())) this.sendMsg(failed + " Either " + parent1.getName() + " or " + parent2.getName() + " is on a breeding cooldown and cannot breed right now!");
-                else if(parent1.getEggGroup().contains(EggGroup.NO_EGGS) || parent2.getEggGroup().contains(EggGroup.NO_EGGS)) this.sendMsg(failed + " Either " + parent1.getName() + " or " + parent2.getName() + " is part of the " + EggGroup.NO_EGGS.getName() + " Egg Group (and cannot breed)!");
-                else if(parent1.getName().equals("Ditto") && parent2.getName().equals("Ditto")) this.sendMsg(failed + " Ditto cannot breed with itself!");
-                else if(!validEggGroup && !parent1.getName().equals(parent2.getName()) && !parent1.getName().equals("Ditto") && !parent2.getName().equals("Ditto")) this.sendMsg(failed + " " + parent1.getName() + " and " + parent2.getName() + " do not share a common Egg Group and therefore cannot breed!");
-                else if((!parent1.getName().equals("Ditto") && parent2.getGender().equals(Gender.UNKNOWN)) && (!parent2.getName().equals("Ditto") && parent1.getGender().equals(Gender.UNKNOWN))) this.sendMsg(failed + " Either " + parent1.getName() + " or " + parent2.getName() + " has an unknown gender and cannot breed!");
-                else if(parent1.getGender().equals(parent2.getGender())) this.sendMsg(failed + " " + parent1.getName() + " and " + parent2.getName() + " are not opposite genders and cannot breed!");
+                if(this.playerData.getOwnedEggIDs().size() >= PokemonEgg.MAX_EGGS) this.response = "You have the maximum number of eggs! To breed more, hatch some of your existing eggs!";
+                else if(UNABLE_TO_BREED.contains(parent1.getUUID()) || UNABLE_TO_BREED.contains(parent2.getUUID())) this.response = failed + " Either " + parent1.getName() + " or " + parent2.getName() + " is on a breeding cooldown and cannot breed right now!";
+                else if(parent1.getEggGroup().contains(EggGroup.NO_EGGS) || parent2.getEggGroup().contains(EggGroup.NO_EGGS)) this.response = failed + " Either " + parent1.getName() + " or " + parent2.getName() + " is part of the " + EggGroup.NO_EGGS.getName() + " Egg Group (and cannot breed)!";
+                else if(parent1.getName().equals("Ditto") && parent2.getName().equals("Ditto")) this.response = failed + " Ditto cannot breed with itself!";
+                else if(!validEggGroup && !parent1.getName().equals(parent2.getName()) && !parent1.getName().equals("Ditto") && !parent2.getName().equals("Ditto")) this.response = failed + " " + parent1.getName() + " and " + parent2.getName() + " do not share a common Egg Group and therefore cannot breed!";
+                else if((!parent1.getName().equals("Ditto") && parent2.getGender().equals(Gender.UNKNOWN)) && (!parent2.getName().equals("Ditto") && parent1.getGender().equals(Gender.UNKNOWN))) this.response = failed + " Either " + parent1.getName() + " or " + parent2.getName() + " has an unknown gender and cannot breed!";
+                else if(parent1.getGender().equals(parent2.getGender())) this.response = failed + " " + parent1.getName() + " and " + parent2.getName() + " are not opposite genders and cannot breed!";
                 else
                 {
                     PokemonEgg egg = PokemonEgg.create(parent1, parent2);
@@ -74,29 +74,29 @@ public class CommandBreed extends Command
                     this.startCooldown(parent1.getUUID());
                     this.startCooldown(parent2.getUUID());
 
-                    this.sendMsg(parent1.getName() + " and " + parent2.getName() + " successfully bred and created an egg!");
+                    this.response = parent1.getName() + " and " + parent2.getName() + " successfully bred and created an egg!";
                 }
             }
         }
         else if(cooldown)
         {
-            if(this.getInt(2) < 1 || this.getInt(2) > this.playerData.getPokemonList().size()) this.sendMsg("Invalid number!");
+            if(this.getInt(2) < 1 || this.getInt(2) > this.playerData.getPokemonList().size()) this.response = "Invalid number!";
             else
             {
                 Pokemon p = Pokemon.build(this.playerData.getPokemonList().get(this.getInt(2) - 1));
 
-                if(!UNABLE_TO_BREED.contains(p.getUUID()) && !BREEDING_COOLDOWNS.containsKey(p.getUUID())) this.sendMsg(p.getName() + " is able to breed!");
+                if(!UNABLE_TO_BREED.contains(p.getUUID()) && !BREEDING_COOLDOWNS.containsKey(p.getUUID())) this.response = p.getName() + " is able to breed!";
                 else
                 {
                     int rawSeconds = (int)BREEDING_COOLDOWNS.get(p.getUUID()).getDelay(TimeUnit.SECONDS);
                     int seconds = rawSeconds % 60;
                     int minutes = rawSeconds / 60;
 
-                    this.sendMsg(p.getName() + " will be able to breed again in `" + minutes + " min " + seconds + " sec`!");
+                    this.response = p.getName() + " will be able to breed again in `" + minutes + " min " + seconds + " sec`!";
                 }
             }
         }
-        else this.sendMsg(CommandInvalid.getShort());
+        else this.response = CommandInvalid.getShort();
 
         return this;
     }
