d93a8d6251393d80c180f29fb6cce2ec3b3db79b
Calculus-Master <sapmallick@gmail.com>
2022-01-11 11:56:25 -0700

    Updated p!start embed Reworked p!start logic p!start now automatically sends p!info for the starter pokemon after execution

--- src/main/java/com/calculusmaster/pokecord/commands/misc/CommandStart.java
+++ src/main/java/com/calculusmaster/pokecord/commands/misc/CommandStart.java
@@ -1,6 +1,7 @@
 package com.calculusmaster.pokecord.commands.misc;
 
 import com.calculusmaster.pokecord.commands.Command;
+import com.calculusmaster.pokecord.commands.pokemon.CommandInfo;
 import com.calculusmaster.pokecord.game.enums.functional.Achievements;
 import com.calculusmaster.pokecord.game.pokemon.Pokemon;
 import com.calculusmaster.pokecord.mongo.PlayerDataQuery;
@@ -24,29 +25,35 @@ public class CommandStart extends Command
     {
         boolean isRegistered = PlayerDataQuery.isRegistered(this.player.getId());
 
-        if(isRegistered)
-        {
-            this.embed.setDescription("You have already started your journey!");
-        }
+        if(isRegistered) this.response = "You have already started your journey!";
         else if(this.msg.length != 2 || !this.msg[0].equals("start"))
         {
-            this.embed.setTitle("Welcome to the world of Pokemon!");
-            this.embed.setDescription("To begin, use p!start <starter>, where <starter> is the name of the starter you wish to pick.\n" +
-                    "Possible Starters: \n" +
-                    "**Bulbasaur** | **Charmander** | **Squirtle**\n" +
-                    "**Chikorita** | **Cyndaquil** | **Totodile**\n" +
-                    "**Treecko** | **Torchic** | **Mudkip**\n" +
-                    "**Turtwig** | **Chimchar** | **Piplup**\n" +
-                    "**Snivy** | **Tepig** | **Oshawott**\n" +
-                    "**Chespin** | **Fennekin** | **Froakie**\n" +
-                    "**Rowlet** | **Litten** | **Popplio**\n" +
-                    "**Grookey** | **Scorbunny** | **Sobble**\n");
-            this.embed.setImage("https://vignette.wikia.nocookie.net/pokeverse/images/4/46/Pokemon_starters_.png/revision/latest/scale-to-width-down/1000?cb=20180424013225");
+            this.embed
+                    .setTitle("Welcome to the world of Pokemon!")
+                    .addField("Starter", """
+                            To being, use the `p!start <starter>` command to select your starter Pokemon. Replace <starter> with the name of the starter you pick.
+                            Possible Starters:\s
+                            Generation 1: **Bulbasaur** | **Charmander** | **Squirtle**
+                            Generation 2: **Chikorita** | **Cyndaquil** | **Totodile**
+                            Generation 3: **Treecko** | **Torchic** | **Mudkip**
+                            Generation 4: **Turtwig** | **Chimchar** | **Piplup**
+                            Generation 5: **Snivy** | **Tepig** | **Oshawott**
+                            Generation 6: **Chespin** | **Fennekin** | **Froakie**
+                            Generation 7: **Rowlet** | **Litten** | **Popplio**
+                            Generation 8: **Grookey** | **Scorbunny** | **Sobble**
+                            """, false)
+                    .addField("Progression", """
+                            This bot uses a Level-based progression system. To unlock certain features, you have to earn enough experience and complete certain tasks in order to advance to the next level.
+                            Check what tasks you have to complete in order to level up by using the `p!level` command.
+                            Upon reaching level 19, you will have unlocked all of the core features of the bot. Level 20 and beyond contains more advanced features that are tougher to unlock!
+                            """, false)
+                    .addField("Help", """
+                            Use `p!help <command name>` to learn how to use certain commands!
+                            Certain advanced commands will additionally have an `info` secondary option that provides a more detailed explanation (For example, `p!duel info` or `p!target info`)
+                            """, false)
+                    .setImage("https://vignette.wikia.nocookie.net/pokeverse/images/4/46/Pokemon_starters_.png/revision/latest/scale-to-width-down/1000?cb=20180424013225");
         }
-        else if(!Global.isStarter(this.msg[1]))
-        {
-            this.embed.setDescription("Please input a valid starter");
-        }
+        else if(!Global.isStarter(this.msg[1])) this.response = "Please input a valid starter!";
         else
         {
             PlayerDataQuery.register(this.player);
@@ -71,7 +78,10 @@ public class CommandStart extends Command
                 this.event.getChannel().sendMessage("You also acquired a " + mythical.getName() + "!").queue();
             }
 
-            this.embed.setDescription("You started your journey with " + starter.getName() + "!");
+            this.response = "You started your journey with " + starter.getName() + "! Check out its stats below!";
+
+            //Run a p!info command on the starter
+            new CommandInfo(this.event, new String[]{"info"}).runCommand().send();
         }
 
         return this;
