9f9982f9968fc87d23bfcba0a1c9f8511e681982
Saptarshi Mallick <sapmallick@gmail.com>
2022-01-16 12:03:18 -0700

    Added ConsecutiveDamageEffect

---
+++ src/main/java/com/calculusmaster/pokecord/game/moves/builder/ConsecutiveDamageEffect.java
@@ -0,0 +1,38 @@
+package com.calculusmaster.pokecord.game.moves.builder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConsecutiveDamageEffect extends FixedDamageEffect
+{
+    private List<Integer> powers;
+
+    public ConsecutiveDamageEffect(int... powers)
+    {
+        this.powers = Arrays.stream(powers).boxed().toList();
+    }
+
+    @Override
+    public String get()
+    {
+        int totalDamage = this.move.getDamage(this.user, this.opponent);
+        int hits = 1;
+
+        for(int power : this.powers.subList(1, this.powers.size()))
+        {
+            this.move.setPower(power);
+
+            if(this.move.isAccurate(this.user, this.opponent))
+            {
+                totalDamage += this.move.getDamage(this.user, this.opponent);
+                hits++;
+            }
+            else break;
+        }
+
+        this.move.setPower(this.powers.get(0));
+
+        this.set(totalDamage);
+        return super.get() + " " + this.user.getName() + " landed " + hits + " hits!";
+    }
+}
