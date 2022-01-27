4ce232227a0581e6381c85e8e03822cffb67484e
Saptarshi Mallick <sapmallick@gmail.com>
2022-01-26 23:34:52 -0700

    Fixed major bugs in TypeEffectiveness with 0 effectiveness matchups Overhauled TypeEffectiveness data storage method Fixed Fighting-Ghost, Poison-Steel, and Dark-Psychic zero effectiveness two-way relationships

--- src/main/java/com/calculusmaster/pokecord/game/moves/TypeEffectiveness.java
+++ src/main/java/com/calculusmaster/pokecord/game/moves/TypeEffectiveness.java
@@ -2,82 +2,64 @@ package com.calculusmaster.pokecord.game.moves;
 
 import com.calculusmaster.pokecord.game.enums.elements.Type;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.calculusmaster.pokecord.game.enums.elements.Type.*;
+
 public class TypeEffectiveness
 {
-    private enum EffectType
+    private enum Effectiveness
     {
-        NORMAL("", "Fighting", "Ghost"),
-        FIRE("Fire-Grass-Ice-Bug-Steel-Fairy", "Water-Ground-Rock"),
-        WATER("Fire-Water-Ice-Steel", "Electric-Grass"),
-        ELECTRIC("Electric-Flying-Steel", "Ground"),
-        GRASS("Water-Electric-Grass-Ground", "Fire-Ice-Poison-Flying-Bug"),
-        ICE("Ice", "Fire-Fighting-Rock-Steel"),
-        FIGHTING("Bug-Rock-Dark", "Flying-Psychic-Fairy"),
-        POISON("Grass-Fighting-Poison-Bug-Fairy", "Ground-Psychic"),
-        GROUND("Poison-Rock", "Water-Grass-Ice", "Electric"),
-        FLYING("Grass-Fighting-Bug", "Electric-Ice-Rock", "Ground"),
-        PSYCHIC("Fighting-Psychic", "Bug-Ghost-Dark"),
-        BUG("Grass-Fighting-Ground", "Fire-Flying-Rock"),
-        ROCK("Normal-Fire-Poison-Flying", "Water-Grass-Fighting-Ground-Steel"),
-        GHOST("Poison-Bug", "Ghost-Dark", "Normal-Fighting"),
-        DRAGON("Fire-Water-Electric-Grass", "Ice-Dragon-Fairy"),
-        DARK("Ghost-Dark", "Fighting-Bug-Fairy", "Psychic"),
-        STEEL("Normal-Grass-Ice-Flying-Psychic-Bug-Rock-Dragon-Steel-Fairy", "Fire-Fighting-Ground", "Poison"),
-        FAIRY("Fighting-Bug-Dark", "Poison-Steel", "Dragon");
+        E_NORMAL(List.of(), List.of(FIGHTING), List.of(GHOST)),
+        E_FIRE(List.of(FIRE, GRASS, ICE, BUG, STEEL, FAIRY), List.of(WATER, GROUND, ROCK)),
+        E_WATER(List.of(FIRE, WATER, ICE, STEEL), List.of(ELECTRIC, GRASS)),
+        E_ELECTRIC(List.of(ELECTRIC, FLYING, STEEL), List.of(GROUND)),
+        E_GRASS(List.of(WATER, ELECTRIC, GRASS, GROUND), List.of(FIRE, ICE, POISON, FLYING, BUG)),
+        E_ICE(List.of(ICE), List.of(FIRE, FIGHTING, ROCK, STEEL)),
+        E_FIGHTING(List.of(BUG, ROCK, DARK), List.of(FLYING, PSYCHIC, FAIRY), List.of(GHOST)),
+        E_POISON(List.of(GRASS, FIGHTING, POISON, BUG, FAIRY), List.of(GROUND, PSYCHIC), List.of(STEEL)),
+        E_GROUND(List.of(POISON, ROCK), List.of(WATER, GRASS, ICE), List.of(ELECTRIC)),
+        E_FLYING(List.of(GRASS, FIGHTING, BUG), List.of(ELECTRIC, ICE, ROCK), List.of(GROUND)),
+        E_PSYCHIC(List.of(FIGHTING, PSYCHIC), List.of(BUG, GHOST, DARK)),
+        E_BUG(List.of(GRASS, FIGHTING, GROUND), List.of(FIRE, FLYING, ROCK)),
+        E_ROCK(List.of(NORMAL, FIRE, POISON, FLYING), List.of(WATER, GRASS, FIGHTING, GROUND, STEEL)),
+        E_GHOST(List.of(POISON, BUG), List.of(GHOST, DARK), List.of(NORMAL, FIGHTING)),
+        E_DRAGON(List.of(FIRE, WATER, ELECTRIC, GRASS), List.of(ICE, DRAGON), List.of(FAIRY)),
+        E_DARK(List.of(GHOST, DARK), List.of(FIGHTING, BUG, FAIRY), List.of(PSYCHIC)),
+        E_STEEL(List.of(NORMAL, GRASS, ICE, FLYING, PSYCHIC, BUG, ROCK, DRAGON, STEEL, FAIRY), List.of(FIRE, FIGHTING, GROUND), List.of(POISON)),
+        E_FAIRY(List.of(FIGHTING, BUG, DARK), List.of(POISON, STEEL), List.of(DRAGON));
 
-        private Map<Type, Double> effectMap;
-        EffectType(String notEffect, String superEffect, String zeroEffect)
+        private final Map<Type, Double> values;
+
+        Effectiveness(List<Type> notEffective, List<Type> superEffective, List<Type> zeroEffective)
         {
-            this.effectMap = new HashMap<>();
+            this.values = new HashMap<>();
 
-            for(Type t : this.getStringAsTypeArray(notEffect)) this.effectMap.put(t, 0.5);
-            for(Type t : this.getStringAsTypeArray(superEffect)) this.effectMap.put(t, 2.0);
-            for(Type t : this.getStringAsTypeArray(zeroEffect)) this.effectMap.put(t, 0.0);
+            for(Type t : notEffective) this.values.put(t, 0.5);
+            for(Type t : superEffective) this.values.put(t, 2.0);
+            for(Type t : zeroEffective) this.values.put(t, 0.0);
 
-            this.assignRemainingTypes();
+            for(Type t : Type.values()) if(!this.values.containsKey(t)) this.values.put(t, 1.0);
         }
 
-        EffectType(String notEffect, String superEffect)
+        Effectiveness(List<Type> notEffective, List<Type> superEffective)
         {
-            this(notEffect, superEffect, "");
+            this(notEffective, superEffective, List.of());
         }
 
-        public Map<Type, Double> getMap()
+        static Effectiveness cast(Type t)
         {
-            return this.effectMap;
-        }
-
-        private Type[] getStringAsTypeArray(String str)
-        {
-            String[] split = str.split("-");
-            Type[] out = new Type[split.length];
-            for(int i = 0; i < split.length; i++) out[i] = Type.cast(split[i]);
-
-            return out;
-        }
-
-        private void assignRemainingTypes()
-        {
-            for(Type t : Type.values()) if(!this.effectMap.containsKey(t)) this.effectMap.put(t, 1.0);
-        }
-
-        public static EffectType getEffectForType(Type t)
-        {
-            for(EffectType eT : values()) if(t.toString().equals(eT.toString())) return eT;
-            throw new IllegalStateException("Something went wrong: TypeEffectiveness.EffectType.class");
+            return Arrays.stream(values()).filter(e -> e.toString().substring("E_".length()).equals(t.toString())).findFirst().orElseThrow();
         }
     }
 
     public static Map<Type, Double> getEffectiveness(List<Type> types)
     {
-        Map<Type, Double> effectiveness = new HashMap<>();
-
-        for(Type t : types) EffectType.getEffectForType(t).getMap().forEach((type, effect) -> effectiveness.put(type, effectiveness.getOrDefault(type, 1.0) * effect));
-
-        return effectiveness;
+        Map<Type, Double> total = new HashMap<>();
+        for(Type t : types) Effectiveness.cast(t).values.forEach((type, effect) -> total.put(type, total.getOrDefault(type, 1.0) * effect));
+        return total;
     }
 }
