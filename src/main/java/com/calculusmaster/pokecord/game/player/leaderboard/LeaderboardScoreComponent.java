0b709128ced5adce1b593b890663c010fb1b1888
Saptarshi Mallick <sapmallick@gmail.com>
2023-04-11 09:48:22 -0700

    Added 5 more leaderboard score components

--- src/main/java/com/calculusmaster/pokecord/game/player/leaderboard/LeaderboardScoreComponent.java
+++ src/main/java/com/calculusmaster/pokecord/game/player/leaderboard/LeaderboardScoreComponent.java
@@ -1,14 +1,20 @@
 package com.calculusmaster.pokecord.game.player.leaderboard;
 
 import com.calculusmaster.pokecord.mongo.PlayerDataQuery;
+import com.calculusmaster.pokecord.util.enums.StatisticType;
 
 import java.util.function.Function;
 
 public enum LeaderboardScoreComponent
 {
-    POKEDEX_REGISTERED("Pokemon Registered", 0.6F, p -> p.getPokedex().getSize()),
-    MASTERY_LEVEL("Pokemon Mastery Level", 0.4F, PlayerDataQuery::getLevel),
+    POKEDEX_REGISTERED("Pokemon Registered", 0.75F, p -> p.getPokedex().getSize()),
+    MASTERY_LEVEL("Pokemon Mastery Level", 0.5F, PlayerDataQuery::getLevel),
     ACHIEVEMENTS("Achievements", 0.8F, p -> p.getAchievements().size()),
+    CREDITS_EARNED("Credits Earned", 0.05F, p -> p.getStatistics().get(StatisticType.CREDITS_EARNED)),
+    FORMS_OWNED("Forms Collected", 0.4F, p -> p.getOwnedForms().size()),
+    MEGAS_OWNED("Mega-Evolutions Collected", 0.7F, p -> p.getOwnedMegas().size()),
+    ZCRYSTALS_OWNED("Z-Crystals Collected", 1.0F, p -> p.getInventory().getZCrystals().size()),
+    DUELS_WON("Duels Won", 0.2F, p -> p.getStatistics().get(StatisticType.PVP_DUELS_WON) + p.getStatistics().get(StatisticType.TRAINER_DUELS_WON) + p.getStatistics().get(StatisticType.ELITE_DUELS_WON) * 2 + p.getStatistics().get(StatisticType.RAIDS_WON) * 2 + p.getStatistics().get(StatisticType.WILD_DUELS_WON) / 2)
 
     ;
 
